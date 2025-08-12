e66a5792b4a191def0a147c35138f73463d85827
Gene Park <98907843+gene-themachine@users.noreply.github.com>
2025-08-12 14:20:13 -0400

    modified cors

--- config/cors.ts
+++ config/cors.ts
@@ -8,7 +8,7 @@ import { defineConfig } from '@adonisjs/cors'
  */
 const corsConfig = defineConfig({
   enabled: true,
-  origin: true,
+  origin: ['https://noted-app.com', 'http://localhost:5173'],
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH'],
   headers: true,
   exposeHeaders: [],
